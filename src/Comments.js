bb81f33e91cd9b86dfcb965a3d8f1b7ec4045112
Seth Baughman <sbaughman1@me.com>
2017-06-14 14:44:29 -0400

    Update and add comment to state

--- src/Comments.js
+++ src/Comments.js
@@ -5,23 +5,40 @@ class Comments extends Component {
     super()
 
     this.state = {
-      comment: ''
+      comment: '',
+      comments: []
     }
 
     this.updateComment = this.updateComment.bind(this)
+    this.addComment = this.addComment.bind(this)
   }
 
   updateComment(ev) {
     this.setState({
       comment: ev.target.value
-    }, () => console.log(this.state))
+    })
+  }
+
+  addComment(ev) {
+    const comment = {
+      timestamp: new Date(),
+      text: this.state.comment
+    }
+    const state = {...this.state}
+    state.comments.push(comment)
+    state.comment = ''
+    this.setState(state, () => console.log(this.state))
   }
 
   render() {
     return (
       <div className="comments">
-        <textarea value={this.state.comment} onChange={this.updateComment}></textarea>
-        <button className="button">Submit comment</button>
+        <textarea
+          value={this.state.comment}
+          onChange={this.updateComment}
+          placeholder="Enter comment here">
+        </textarea>
+        <button onClick={this.addComment} className="button">Submit comment</button>
       </div>
     )
   }
