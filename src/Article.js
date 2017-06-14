7db326e695eb22f5902a37c6aba2e0b015fa6041
Seth Baughman <sbaughman1@me.com>
2017-06-14 10:04:16 -0400

    Toggle Comment rendering

--- src/Article.js
+++ src/Article.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 import './Article.css'
 
 class Article extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showComments: false
+    }
+
+    this.toggleComments = this.toggleComments.bind(this)
+  }
+
+  toggleComments(ev) {
+    ev.preventDefault()
+    this.setState({
+      showComments: !this.state.showComments
+    })
+  }
+
   render() {
     return (
       <div className="large-8 medium-12 columns article">
@@ -29,7 +46,7 @@ class Article extends Component {
           <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
         </section>
         <div className="article-links">
-          <a className="article-link" href="#">
+          <a className="article-link" href="#" onClick={this.toggleComments}>
             <i className="fa fa-comments-o"></i>
             <span className="article-link-text">Comments</span>
           </a>
@@ -38,6 +55,7 @@ class Article extends Component {
             <span className="article-link-text">Share Post</span>
           </a>
         </div>
+        {this.state.showComments ? <p>Comments</p> : null}
       </div>
     )
   }
--- src/OtherArticles.js
+++ src/OtherArticles.js
@@ -29,7 +29,7 @@ class OtherArticles extends Component {
     return (
       <div className="small-12 columns other-articles">
         <h2>From around the Realm</h2>
-        {articleData.map(article => <ArticleLink article={article} />)}
+        {articleData.map((article, i) => <ArticleLink key={i} article={article} />)}
       </div>
     )
   }
